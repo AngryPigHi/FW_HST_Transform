64135bd3b8cffde5787d737a1db1f67ad96f80c5
Blithe_Zhu <Blithe_Zhu@LAPTOP-S3I458E5>
2021-09-01 18:55:34 +0800

    添加项目文件。

---
+++ FW_HST_Transform/ConfigFrm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FW_HST_Transform
+{
+    public partial class ConfigFrm : Form
+    {
+        public ConfigFrm()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ FW_HST_Transform/TransformFrm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FW_HST_Transform
+{
+    public partial class TransformFrm : Form
+    {
+        public TransformFrm()
+        {
+            InitializeComponent();
+        }
+    }
+}
